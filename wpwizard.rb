29b9ba830f48e2b1dfee46f1e1a0145c5392eee0
Brad Parbs <brad@bradparbs.com>
2013-02-26 23:40:52 -0600

    sha sums are hard apparently
    Signed-off-by: Brad Parbs <brad@bradparbs.com>

--- wpwizard.rb
+++ wpwizard.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Wpwizard < Formula
   homepage 'https://github.com/bradp/wpWizard'
   url 'https://github.com/bradp/wpWizard/archive/1.0.5.zip'
-  sha1 'e38feba87605e119dc48e0efc88932e25c99808e'
+  sha1 '9ef09ec4871b1c379e99727f347c1a92c513c193'
   version '1.0.5'
   
   def install
