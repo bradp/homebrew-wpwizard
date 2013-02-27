b56d184aac6a463a6bf0240fbc7728bb479e6893
Brad Parbs <brad@bradparbs.com>
2013-02-26 23:28:03 -0600

    updated to hopefully fix conf files and v1.0.4
    Signed-off-by: Brad Parbs <brad@bradparbs.com>

--- wpwizard.rb
+++ wpwizard.rb
@@ -2,11 +2,13 @@ require 'formula'
 
 class Wpwizard < Formula
   homepage 'https://github.com/bradp/wpWizard'
-  url 'https://github.com/bradp/wpWizard/archive/1.0.2.zip'
-  sha1 'c595bcbf5ceb8d61a383a13b27005a04d0b28839'
-  version '1.0.2'
+  url 'https://github.com/bradp/wpWizard/archive/1.0.3.zip'
+  sha1 '8b202f0992113ac6978ddbd4520851adab6b38f8'
+  version '1.0.3'
   
   def install
     bin.install "wpwizard" => "wpwizard"
+    bin.install "conf/plugins.conf" => "conf/plugins.conf"
+    bin.install "conf/mu-plugins.conf" => "conf/mu-plugins.conf"
   end
 end
