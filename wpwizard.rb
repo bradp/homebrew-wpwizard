9ba6c8a38ba083476bc2cfeddd90d44e9234687b
Brad Parbs <brad@bradparbs.com>
2013-02-28 19:21:09 -0600

    updated to v1.1.1

--- wpwizard.rb
+++ wpwizard.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Wpwizard < Formula
   homepage 'https://github.com/bradp/wpWizard'
-  url 'https://github.com/bradp/wpWizard/archive/1.1.0.zip'
-  sha1 '6d292f6c6d5db017b1c01ea40017cab73732d87c'
-  version '1.1.0'
+  url 'https://github.com/bradp/wpWizard/archive/1.1.1.zip'
+  sha1 'cb081f4aaf2e013f536142d0e15bf11f672f2385'
+  version '1.1.1'
   
   def install
     bin.install "wpwizard" => "wpwizard"
